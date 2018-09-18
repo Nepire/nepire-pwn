556942f9f9b229fd430992e7ad9f1e1c211b65db
Nepire <37898081+Nepire@users.noreply.github.com>
2018-09-18 20:50:43 +0800

    9.18

---
+++ install.py
@@ -0,0 +1,43 @@
+import os
+
+def build_images():
+	os.system('docker build -t pwn ~/nepire-pwn/')
+	os.system('docker run -dit --name nepire-pwn pwn')
+	#build images
+
+def create_bin():
+	os.system('touch ~/nepire-pwn/nepire-pwn')
+	os.system('touch ~/nepire-pwn/link-nepire-pwn')
+	os.system('echo "docker cp ~/nepire-pwn/process nepire-pwn:/home/ctf/process" > ~/nepire-pwn/link-nepire-pwn')
+	os.system('echo "docker exec -it nepire-pwn /usr/bin/fish" >> ~/nepire-pwn/link-nepire-pwn')
+	os.system('echo "sh ~/nepire-pwn/link-nepire-pwn" > ~/nepire-pwn/nepire-pwn')
+	os.system('chmod 777 ~/nepire-pwn/link-nepire-pwn')
+	os.system('chmod 777 ~/nepire-pwn/nepire-pwn')
+	os.system('cp ~/nepire-pwn/nepire-pwn /usr/bin/nepire-pwn')
+
+def nepire_pwn():
+	over = '''
+			 _   _ _____ ____ ___ ____  _____                          
+			| \ | | ____|  _ \_ _|  _ \| ____|     _ ____      ___ __  
+			|  \| |  _| | |_) | || |_) |  _| _____| '_ \ \ /\ / / '_ \ 
+			| |\  | |___|  __/| ||  _ <| |__|_____| |_) \ V  V /| | | |
+			|_| \_|_____|_|  |___|_| \_\_____|    | .__/ \_/\_/ |_| |_|
+			                                      |_|                  
+	'''
+	print over
+
+def version():
+	version = '1.0'
+
+	print version
+
+def main():
+	build_images()
+	create_bin()
+	nepire_pwn()
+	
+
+if __name__ == "__main__":
+    main()
+
+
